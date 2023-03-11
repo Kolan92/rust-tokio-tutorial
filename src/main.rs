a445ad97e8bd3125658dd9ef89992fcd18752075
Kolan92 <pawel.krzysztof.kolanowski@gmail.com>
2023-03-11 23:20:04 +0100

    Share state across connections

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
+
+use bytes::Bytes;
+use mini_redis::Command::{self, Get, Set};
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
-pub type Error = Box<dyn std::error::Error + Send + Sync>;
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
+    let db = Arc::new(Mutex::new(HashMap::new()));
     loop {
         let (socket, _) = listener.accept().await?;
-        tokio::spawn(async move { process(socket).await });
+        let db = db.clone();
+        tokio::spawn(async move { process(socket, db).await });
     }
 }
 
-async fn process(socket: TcpStream) -> Result<(), Error> {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
+async fn process(socket: TcpStream, db: Db) -> Result<(), Error> {
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection.read_frame().await? {
         let response = match Command::from_frame(frame).unwrap() {
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
@@ -28,7 +36,8 @@ async fn process(socket: TcpStream) -> Result<(), Error> {
             }
 
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             cmd => panic!("unimplemented {:?}", cmd),
